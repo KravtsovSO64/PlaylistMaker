eebb6b12d7f9ffbbe1270e715b1aa912dd764a08
Sergey Kravtsov <kraffc@yandex.ru>
2024-05-14 12:22:56 +0400

    Hello, my friend! It is my first app!

---
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,20 @@
+package com.practicum.playlistmaker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        val arrowBack = findViewById<ImageView>(R.id.arrow_back)
+
+        arrowBack.setOnClickListener {
+            val returnMain = Intent(this, MainActivity::class.java)
+            startActivity(returnMain)
+        }
+    }
+}
