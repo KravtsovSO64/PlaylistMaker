eb67e8652df251c7e0078386574a404406d4c902
Sergey Kravtsov <kraffc@yandex.ru>
2024-06-03 21:22:04 +0400

    Постарался исправить все замечания!

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -3,12 +3,19 @@ package com.practicum.playlistmaker
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.view.View
 import android.widget.ImageView
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val comeBackMain = findViewById<ImageView>(R.id.arrow_back)
+        comeBackMain.setOnClickListener {
+            finish()
+        }
 
     }
 }
