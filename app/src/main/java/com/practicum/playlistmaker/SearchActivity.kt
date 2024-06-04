6e079384dac48065b22f93aff07aa6c247f4e14e
Sergey Kravtsov <kraffc@yandex.ru>
2024-06-04 09:56:49 +0400

    Новые изменения

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,21 +1,53 @@
 package com.practicum.playlistmaker
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import android.widget.EditText
 import android.widget.ImageView
 
 class SearchActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
         val comeBackMain = findViewById<ImageView>(R.id.arrow_back)
+        val lineSearchLine = findViewById<EditText>(R.id.editText)
+        val clearButtonSearch = findViewById<ImageView>(R.id.clearIcon)
+
+        clearButtonSearch.setOnClickListener{
+            lineSearchLine.setText("")
+        }
         comeBackMain.setOnClickListener {
             finish()
         }
 
+        val searchTextWatcher = object :TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                //Empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButtonSearch.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                //Empty
+            }
+
+        }
+        lineSearchLine.addTextChangedListener(searchTextWatcher)
+    }
+     fun clearButtonVisibility(s : CharSequence?) : Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
     }
 }
